c55c3f269b787f6c11c7b4aae60ba783022ede48
Thabo-Hacker5020 <157540561+Thabo-Hacker5020@users.noreply.github.com>
2024-01-30 11:02:28 +0200

    Add files via upload

---
+++ pandafile1.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 29 12:12:51 2024
+
+@author: thabo
+"""
+
+import pandas
+
+file = pandas.read_csv("country_data.csv")
+
+print(file)
+
+
+print(file.info())
+
+print(file.describe())
+
+
+print(file.describe())
+
+B1=30
+B2=40
+B3=30
+B4=49
+
+print(B1)
+print(B2)
+
+
+
+age1 = 30
+age2 = 25
+age3 = 29
+
+age = [30,25,29,46,22]
+
+print(age)
+
+"""
+
+[30,25,29,46,22]
+
+"""
+print(age[0])
+
+"""
+30
+"""
+print(age[1])
+print(B1)
+print(B4)
+print(age1)
+print(age1)
+print(min(age))
+avg = sum(age)/len(age)
+print(avg)
+g1 = "M"
+g2 = "F"
+g3 = "M"
+
+gender = ["M","F","F"]
+
+c1 = "south Africa"
+c2 = "lesotho"
+print(age[0:2])
+print(age[0:4])
+age.append(100)
+
+print(age)
+print(g2)
+age.append(100)
+f1 = "apple"
+f2 = "banana"
+f3 = "pear"
+print(f1)
+fruits = ["apple","banana","pear"]
+
+mammals = {"cat":"a cute animal","lion":"king of the jungle","elephant":"a gigantic herbivore"}
+print(mammals["cat"])   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ pyfile01.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 29 12:00:03 2024
+
+@author: thabo
+"""
+
+print("my first python file")
+#this is my first comment
+density = 1000
+Density = 900
+mass_kg = 50
+#three main types of data
+"""
+1. int - integer - whole number
+2. float - number number with decimals
+3. strings - letters, text, words, paragraphs
+"""
+
+gravity_m_2_sec = 9.8
+water_level_m_above_sea_level = 14.89
+
+pi = 3.14
+
+fruit = "banana"
+
+element = "potassium"
+
+parasite = "plasmodium"
+
+delivered_prematurely = "6 months, <8 months"
+
